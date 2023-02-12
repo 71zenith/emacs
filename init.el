d99f03e4476fceb8d6ef18b8e66bff1294d2dafb
zen <71zenith@proton.me>
2023-02-12 10:39:16 +0530

    refactor: idk what im doing, but it will work soon :tm:

--- init.el
+++ init.el
@@ -1,11 +1,13 @@
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" "19a2c0b92a6aa1580f1be2deb7b8a8e3a4857b6c6ccf522d00547878837267e7" "37046960cf667c5ab3b76235d35a5db4763c531e706502a9067fa78db5a775c0" default))
  '(package-selected-packages
-   '(evil-collection magit evil which-key dashboard marginalia org-bullets)))
+   '(gruvbox-theme atom-one-dark-theme evil-collection magit evil which-key dashboard marginalia org-bullets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
