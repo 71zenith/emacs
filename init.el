0f502de2e3d50ac39d1466849aa8d0571492f487
zen <71zenith@proton.me>
2025-02-20 14:22:04 +0530

    tweaks

--- init.el
+++ init.el
@@ -20,15 +20,16 @@
       use-package-compute-statistics t
       use-package-minimum-reported-time 0.1)
 
-(use-package diminish :defer t)
-(use-package no-littering :demand t)
+(use-package diminish :ensure t :defer t)
+(use-package no-littering :ensure t :demand t)
 
 ;;; Undo System
-(use-package undo-fu :defer t)
-(use-package undo-fu-session :defer t :hook (after-init . global-undo-fu-session-mode))
+(use-package undo-fu :ensure t :defer t)
+(use-package undo-fu-session :ensure t :defer t :hook (after-init . global-undo-fu-session-mode))
 
 ;;; Evil Mode
 (use-package evil
+  :ensure t
   :init
   (setq evil-want-keybinding nil
         evil-want-C-u-scroll t
@@ -36,22 +37,22 @@
         evil-undo-system 'undo-fu)
   :config (evil-mode 1))
 
-(use-package evil-collection
+(use-package evil-collection :ensure t
   :after evil
   :diminish evil-collection-unimpaired-mode
   :config (evil-collection-init))
 
-(use-package evil-surround
+(use-package evil-surround :ensure t
   :after evil
   :hook (after-init . global-evil-surround-mode))
 
-(use-package evil-commentary
+(use-package evil-commentary :ensure t
   :after evil
   :diminish evil-commentary-mode
   :hook (after-init . evil-commentary-mode))
 
 ;;; General.el
-(use-package general
+(use-package general :ensure t
   :demand t
   :config
   (general-evil-setup)
@@ -118,19 +119,19 @@
 (global-set-key (kbd "<escape>") 'keyboard-quit)
 
 ;;; Visual Elements
-(use-package which-key
+(use-package which-key :ensure t
   :defer t
   :diminish which-key-mode
   :hook (after-init . which-key-mode))
 
-(use-package which-key-posframe
+(use-package which-key-posframe :ensure t
   :defer t
   :hook (which-key-mode . which-key-posframe-mode)
   :custom (which-key-posframe-parameters '((left-fringe . 5) (right-fringe . 5))))
 
-(use-package base16-theme :demand t :config (load-theme 'base16-oxocarbon-dark t))
+(use-package base16-theme :ensure t :demand t :config (load-theme 'base16-oxocarbon-dark t))
 
-(use-package spacious-padding
+(use-package spacious-padding :ensure t
   :defer t
   :hook (after-init . spacious-padding-mode)
   :custom
@@ -138,23 +139,23 @@
    '(:internal-border-width 8
                             :mode-line-width 1
                             :tab-width 2
-                            :left-fringe-width 4
+                            :fringe-width 6
                             :right-divider-width 10)))
 
-(use-package rainbow-delimiters :defer t :hook (prog-mode . rainbow-delimiters-mode))
-(use-package highlight-numbers :defer t :hook (prog-mode . highlight-numbers-mode))
+(use-package rainbow-delimiters :ensure t :defer t :hook (prog-mode . rainbow-delimiters-mode))
+(use-package highlight-numbers :ensure t :defer t :hook (prog-mode . highlight-numbers-mode))
 
-(use-package popper
+(use-package popper :ensure t
   :defer t
   :hook ((after-init . popper-mode)
          (after-init . popper-echo-mode))
   :bind (("C-`"   . popper-toggle))
-  :config
-  (setq popper-reference-buffers '("\\*.*\\*")
-        popper-mode-line `(:eval (propertize " POP" 'face '(:foregorund ,(face-foreground 'font-lock-warning-face) :weight bold)))))
+  :custom
+  (popper-reference-buffers '("\\*.*\\*"))
+  (popper-mode-line `(:eval (propertize " POP" 'face '(:foregorund ,(plist-get base16-oxocarbon-dark-theme-colors :base0D) :weight bold)))))
 
 ;;; Completion
-(use-package cape
+(use-package cape :ensure t
   :config
   (add-hook 'completion-at-point-functions #'cape-dabbrev)
   (add-hook 'completion-at-point-functions #'cape-file)
@@ -162,7 +163,7 @@
   (add-hook 'completion-at-point-functions #'cape-history)
   (add-hook 'completion-at-point-functions #'cape-elisp-block))
 
-(use-package corfu
+(use-package corfu :ensure t
   :hook ((after-init . global-corfu-mode)
          (after-init . corfu-history-mode)
          (after-init . corfu-popupinfo-mode))
@@ -175,47 +176,40 @@
         corfu-popupinfo-delay '(0.5 . 0.25)))
 
 ;;; Minibuffer
-(use-package vertico :hook (after-init . vertico-mode))
-(use-package vertico-posframe
+(use-package vertico :ensure t :hook (after-init . vertico-mode))
+(use-package vertico-posframe :ensure t
   :hook (vertico-mode . vertico-posframe-mode)
   :custom (vertico-posframe-parameters '((left-fringe . 5) (right-fringe . 5))))
 
-(use-package marginalia :hook (after-init . marginalia-mode))
+(use-package marginalia :ensure t :hook (after-init . marginalia-mode))
 
-(use-package orderless
-  :config
-  (setq completion-styles '(orderless substring)
-        completion-category-overrides '((file (styles partial-completion)))))
+(use-package orderless :ensure t
+  :custom
+  (completion-styles '(orderless substring))
+  (completion-category-overrides '((file (styles partial-completion)))))
 
-(use-package consult
+(use-package consult :ensure t
   :config
   (setq xref-show-xrefs-function #'consult-xref
         xref-show-definitions-function #'consult-xref)
   (global-set-key [remap project-switch-to-buffer] 'consult-project-buffer)
   (global-set-key [remap isearch-forward] 'consult-line))
 
-(use-package helpful :defer t)
+(use-package helpful :ensure t :defer t)
 
 ;;; VC
-(use-package magit :defer t)
-(use-package transient-posframe :defer t :hook (after-init . transient-posframe-mode))
-(use-package diff-hl
+(use-package magit :ensure t :defer t)
+(use-package diff-hl :ensure t
   :defer t
   :hook (after-init . global-diff-hl-mode)
   :diminish diff-hl-mode)
 
-(use-package flycheck-eglot
-  :ensure t
-  :after (flycheck eglot)
-  :config
-  (global-flycheck-eglot-mode 1))
-
 ;;; Lang Support
-(use-package nix-mode :defer t)
-(use-package zig-mode :defer t :custom (zig-format-on-save nil))
+(use-package nix-mode :ensure t :defer t)
+(use-package zig-mode :ensure t :defer t :custom (zig-format-on-save nil))
 
 ;;; LSP Support
-(use-package eglot
+(use-package eglot :ensure t
   :defer t
   :hook (zig-mode . eglot-ensure)
   :config
@@ -223,37 +217,44 @@
   (setq eglot-events-buffer-size 0
         eglot-send-changes-idle-time 0.1))
 
-(use-package eglot-booster
+(use-package eglot-booster :ensure t
   :after eglot
   :vc (:url "https://github.com/jdtsmith/eglot-booster" :rev :newest :branch "main")
   :hook (eglot-booster-mode . eglot-ensure))
 
-(use-package yasnippet
+(use-package yasnippet :ensure t
   :defer t
   :diminish yas-minor-mode
   :hook (prog-mode . yas-minor-mode))
 
+(use-package nerd-icons :ensure t)
+
 ;;; Diagnostics
-(use-package flymake
+(use-package flymake :ensure t
   :defer t
+  :custom
+  (flymake-indicator-type 'margins)
+  (flymake-margin-indicators-string `((error "X" compilation-error)
+                                      (warning "!" compilation-warning)
+                                      (note "i" compilation-info)))
   :config
   (custom-set-faces
    '(flymake-error   ((t (:underline (:style wave :color "Red")))))
    '(flymake-warning ((t (:underline (:style wave :color "Orange")))))
    '(flymake-note    ((t (:underline (:style wave :color "Blue")))))))
 
-(use-package eldoc
+(use-package eldoc :ensure t
   :defer t
   :hook (prog-mode . eldoc-mode)
   :diminish eldoc-mode)
 
-(use-package eldoc-box
+(use-package eldoc-box :ensure t
   :defer t
   :hook (eldoc-mode . eldoc-box-hover-mode)
   :diminish eldoc-box-hover-mode
   :custom (eldoc-idle-delay 0.1))
 
-(use-package copilot-chat
+(use-package copilot-chat :ensure t
   :defer t
   :custom
   (copilot-chat-frontend 'shell-maker))
@@ -264,18 +265,18 @@
   :hook
   ((dired-mode . dired-hide-details-mode)
    (dired-mode . hl-line-mode))
-  :config
-  (setq dired-recursive-copies 'always
-        dired-recursive-deletes 'always
-        dired-mouse-drag-files t
-        delete-by-moving-to-trash t
-        dired-dwim-target t))
+  :custom
+  (dired-recursive-copies 'always)
+  (dired-recursive-deletes 'always)
+  (dired-mouse-drag-files t)
+  (delete-by-moving-to-trash t)
+  (dired-dwim-target t))
 
-(use-package async
+(use-package async :ensure t
   :defer t
   :hook (dired-mode . dired-async-mode))
 
-(use-package dired-subtree
+(use-package dired-subtree :ensure t
   :defer t :after dired
   :bind
   (:map dired-mode-map
@@ -286,29 +287,27 @@
   :custom (dired-subtree-use-backgrounds nil))
 
 ;;; Terminal
-(use-package eat
+(use-package eat :ensure t
   :defer t
   :hook ((eshell-mode . eat-eshell-mode)
          (eshell-mode . eat-eshell-visual-command-mode)))
 
-(use-package eshell-syntax-highlighting
+(use-package eshell-syntax-highlighting :ensure t
   :defer t
   :hook (eshell-mode . eshell-syntax-highlighting-mode))
 
 (use-package eshell
   :defer t
   :hook (eshell-mode . (lambda () (eshell/alias "c" "clear-scrollback")))
-  :config
-  (setq eshell-banner-message ""
-        eshell-prompt-function
-        (lambda nil
-          (let ((dir-color (face-attribute 'font-lock-keyword-face :foreground))
-                (prompt-color (face-attribute 'font-lock-builtin-face :foreground)))
-            (concat
-             (propertize (abbreviate-file-name (eshell/pwd)) 'face `(:foreground ,dir-color))
-             (propertize " λ" 'face `(:foreground ,prompt-color))
-             (propertize " "))))
-        ))
+  :custom
+  (eshell-banner-message "")
+  (eshell-prompt-function
+   (lambda nil
+     (concat
+      (propertize (abbreviate-file-name (eshell/pwd)) 'face `(:foreground ,(plist-get base16-oxocarbon-dark-theme-colors :base09)))
+      (propertize " λ" 'face `(:foreground ,(plist-get base16-oxocarbon-dark-theme-colors :base0A)))
+      (propertize " ")))
+   ))
 
 ;;; Global Modes
 (dolist (mode '(global-hl-line-mode
@@ -350,6 +349,7 @@
  ;; Display settings
  display-time-default-load-average nil
  fringes-outside-margins nil
+ fringe-indicator-alist nil
  indicate-buffer-boundaries nil
  indicate-empty-lines nil
  line-spacing 0.08
@@ -401,15 +401,14 @@
  mode-line-right-align-edge 'right-fringe
  mode-line-format
  `(
-   (:eval (propertize (format " [%s]" (substring current-input-method 0 2)) 'face '(:foreground ,(face-foreground 'font-lock-preprocessor-face) :weight bold)))
-   (:eval (propertize evil-mode-line-tag 'face '(:foreground ,(face-foreground 'font-lock-regexp-grouping-construct))))
-   (:eval (propertize " %I " 'face '(:foreground ,(face-foreground 'elisp-shorthand-font-lock-face))))
-   (:eval (propertize " %l:%c " 'face '(:foreground ,(face-foreground 'font-lock-type-face))))
-   (:eval (propertize " %p " 'face '(:foreground ,(face-foreground 'font-lock-builtin-face))))
-   (:eval (propertize (format "%s  " (project-mode-line-format)) 'face '(:foreground ,(face-foreground 'font-lock-string-face) :weight bold)))
+   (:eval (propertize (format " [%s]" (substring current-input-method 0 2)) 'face '(:foreground ,(plist-get base16-oxocarbon-dark-theme-colors :base0D) :weight bold)))
+   (:eval (propertize evil-mode-line-tag 'face '(:foreground ,(plist-get base16-oxocarbon-dark-theme-colors :base0E))))
+   (:eval (propertize " %I " 'face '(:foreground ,(plist-get base16-oxocarbon-dark-theme-colors :base0B))))
+   (:eval (propertize " %l:%c " 'face '(:foreground ,(plist-get base16-oxocarbon-dark-theme-colors :base07))))
+   (:eval (propertize " %p " 'face '(:foreground ,(plist-get base16-oxocarbon-dark-theme-colors :base0D))))
+   (:eval (propertize (format "%s  " (project-mode-line-format)) 'face '(:foreground ,(plist-get base16-oxocarbon-dark-theme-colors :base09) :weight bold)))
    flymake-mode-line-counters
    mode-line-format-right-align
-   (:eval (propertize (format " %s " (capitalize (substring vc-mode 5))) 'face '(:foreground ,(face-foreground 'font-lock-string-face) :weight bold)))
-   (:eval (propertize " %b  " 'face '(:foreground ,(face-foreground 'font-lock-type-face) :weight bold)))
-   (:eval (propertize (capitalize (symbol-name major-mode)) 'face '(:foreground ,(face-foreground 'font-lock-function-name-face) :weight bold)))
-   ))
+   (:eval (propertize (format " %s " (capitalize (substring vc-mode 5))) 'face '(:foreground ,(plist-get base16-oxocarbon-dark-theme-colors :base0B) :weight bold)))
+   (:eval (propertize " %b  " 'face '(:foreground ,(plist-get base16-oxocarbon-dark-theme-colors :base0A) :weight bold)))
+   (:eval (propertize (capitalize (symbol-name major-mode)) 'face '(:foreground ,(plist-get base16-oxocarbon-dark-theme-colors :base0D) :weight bold)))))
